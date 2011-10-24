c33b0f9002df7541ff2864c5c4981a8f0a72aa0f
alex lop <alexlop@iMac-de-Alexis-Lopater.local>
2011-10-24 23:56:07 +0200

    menu et form
    modifications couleurs
    ajout tableau dans forme ajout

--- gestion/minimenu.php
+++ gestion/minimenu.php
@@ -1,7 +1,7 @@
 <?php 
 $hosting = "http://" . $_SERVER['HTTP_HOST'];
 $texto .= "
-<li class=\"menu\"><a href=\"$hosting/stock/gestion/titre\" >Titre</a> | </li>
+<li class=\"menu jfk-button\"><a href=\"$hosting/stock/gestion/titre\" >Titre</a> | </li>
 <li class=\"menu\"><a href=\"$hosting/stock/gestion/categorie\" >Cat&eacute;gories</a> | </li>
 <li class=\"menu\"><a href=\"$hosting/stock/gestion/fournisseur\" >Fournisseur</a> | </li>
 <li class=\"menu\"><a href=\"$hosting/stock/gestion/produit\" >Produits</a> | </li>
--- gestion6/minimenu.php
+++ gestion6/minimenu.php
@@ -1,10 +1,10 @@
 <?php 
 $hosting = "http://" . $_SERVER['HTTP_HOST'];
 $minimenu .= "
-<li class=\"menu\"><a href=\"$hosting/stock3/gestion6/titre\" >Titre</a> | </li>
-<li class=\"menu\"><a href=\"$hosting/stock3/gestion6/categorie\" >Cat&eacute;gories</a> | </li>
-<li class=\"menu\"><a href=\"$hosting/stock3/gestion6/fournisseur\" >Fournisseur</a> | </li>
-<li class=\"menu\"><a href=\"$hosting/stock3/gestion6/produit\" >Produits</a> | </li>
-<li class=\"menu\"><a href=\"$hosting/stock/torque/option\" >Option torque</a></li>
-</ul><br /><br />";
+<li class=\"menu\"><a class=\"menu\" href=\"$hosting/stock/gestion6/titre\" >Titre</a> </li>
+<li class=\"menu\"><a class=\"menu\" href=\"$hosting/stock/gestion6/categorie\" >Cat&eacute;gories</a> </li>
+<li class=\"menu\"><a class=\"menu\" href=\"$hosting/stock/gestion6/fournisseur\" >Fournisseurs</a> </li>
+<li class=\"menu\"><a class=\"menu\" href=\"$hosting/stock/gestion6/produit\" >Produits</a> </li>
+<li class=\"menu\"><a class=\"menu\" href=\"$hosting/stock/torque/option\" >Option torque</a></li>
+</ul>";
 ?>
